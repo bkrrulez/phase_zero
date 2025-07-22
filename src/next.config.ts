00ccb40f7a93ecf9e46a41999e357a90179e1330
Bikramjit Chowdhury <bkrrulez@gmail.com>
2025-07-22 15:50:51 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- src/next.config.ts
+++ src/next.config.ts
@@ -20,6 +20,19 @@ const nextConfig: NextConfig = {
     ],
   },
   devIndicators: false,
+  webpack: (config, { isServer }) => {
+    // Exclude the scripts directory from being watched
+    if (!isServer) {
+        config.watchOptions = {
+            ...config.watchOptions,
+            ignored: [
+                ...(config.watchOptions.ignored as any[] || []),
+                '**/scripts/**'
+            ],
+        }
+    }
+    return config;
+  }
 };
 
 export default nextConfig;
