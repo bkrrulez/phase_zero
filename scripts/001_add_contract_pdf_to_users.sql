6cc767cae60e953f0debddd4d82282c456f4e055
Bikramjit Chowdhury <bkrrulez@gmail.com>
2025-07-26 02:32:36 +0000

    stable version 1.2

--- scripts/001_add_contract_pdf_to_users.sql
+++
@@ -1,2 +0,0 @@
--- Add the contract_pdf column to the users table to store contract documents.
-ALTER TABLE users ADD COLUMN contract_pdf TEXT;
--- scripts/001_add_recurring_day_column.sql
+++
@@ -1,3 +0,0 @@
--- Adds the recurring_day column to the freeze_rules table for recurring freezes.
--- The IF NOT EXISTS clause prevents errors if the column has already been added.
-ALTER TABLE freeze_rules ADD COLUMN IF NOT EXISTS recurring_day INTEGER;
