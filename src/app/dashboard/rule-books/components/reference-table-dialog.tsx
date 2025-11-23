fbfa926e963ee150bd1496cb046468ebd4b5f2b9
Bikramjit Chowdhury <bkrrulez@gmail.com>
2025-11-23 18:31:20 +0000

    why same issue is coming yet again. increase the width of popup if that'

--- src/app/dashboard/rule-books/components/reference-table-dialog.tsx
+++ src/app/dashboard/rule-books/components/reference-table-dialog.tsx
@@ -11,7 +11,6 @@ import {
 } from '@/components/ui/dialog';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import { type ReferenceTable } from '@/lib/types';
-import { ScrollArea } from '@/components/ui/scroll-area';
 import { useLanguage } from '../../contexts/LanguageContext';
 import { LatexRenderer } from './latex-renderer';
 
@@ -31,35 +30,37 @@ export function ReferenceTableDialog({ isOpen, onOpenChange, table }: ReferenceT
 
     return (
         <Dialog open={isOpen} onOpenChange={onOpenChange}>
-            <DialogContent className="max-w-4xl">
+            <DialogContent className="max-w-6xl flex flex-col h-[90vh]">
                 <DialogHeader>
                     <DialogTitle>{t('referenceTableTitle', { name: table.name })}</DialogTitle>
                     <DialogDescription>
                         {t('referenceTableDesc')}
                     </DialogDescription>
                 </DialogHeader>
-                <ScrollArea className="max-h-[70vh] w-full p-4">
-                    <Table>
-                        <TableHeader>
-                            <TableRow>
-                                {headers.map(header => (
-                                    <TableHead key={header} className="max-w-[200px] break-words">{header}</TableHead>
-                                ))}
-                            </TableRow>
-                        </TableHeader>
-                        <TableBody>
-                            {bodyRows.map((row, rowIndex) => (
-                                <TableRow key={rowIndex}>
-                                    {Array.isArray(row) && (row as string[]).map((cell, cellIndex) => (
-                                        <TableCell key={`${rowIndex}-${cellIndex}`} className="max-w-[200px] break-words align-top">
-                                            <LatexRenderer text={String(cell ?? '')} />
-                                        </TableCell>
+                <div className="flex-1 min-h-0">
+                    <div className="relative h-full w-full overflow-auto">
+                        <Table>
+                            <TableHeader className="sticky top-0 bg-background z-10">
+                                <TableRow>
+                                    {headers.map((header, index) => (
+                                        <TableHead key={`${header}-${index}`} className="whitespace-nowrap">{header}</TableHead>
                                     ))}
                                 </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </ScrollArea>
+                            </TableHeader>
+                            <TableBody>
+                                {bodyRows.map((row, rowIndex) => (
+                                    <TableRow key={rowIndex}>
+                                        {Array.isArray(row) && (row as string[]).map((cell, cellIndex) => (
+                                            <TableCell key={`${rowIndex}-${cellIndex}`} className="align-top whitespace-nowrap">
+                                                <LatexRenderer text={String(cell ?? '')} />
+                                            </TableCell>
+                                        ))}
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </div>
+                </div>
             </DialogContent>
         </Dialog>
     );
